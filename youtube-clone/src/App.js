b450d32542316e8b78eaf1b7e08e86b2b3a1ac78
Pankhuri <pankhuritrikha@gmail.com>
2020-07-15 20:50:40 +0530

    Whole structure is done

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Grid } from '@material-ui/core'; // curly braces around means tht this is not a default export but youtube is
+import youtube from './api/youtube';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Youtube Clone App</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* Search Bar */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/* Video detail */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* Video List */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         );
     }
 }
