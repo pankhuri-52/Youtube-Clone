6203e680cd307965f76023eea3206c385d76f6e9
Pankhuri <pankhuritrikha@gmail.com>
2020-07-16 18:07:08 +0530

    Api working fine now

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Grid } from '@material-ui/core'; // curly braces around means tht this is not a default export but youtube is
-//import youtube from './api/youtube';
+import youtube from './api/youtube';
 import {SearchBar, VideoDetail} from './components';
 
 class App extends React.Component {
+
+    //async stops the execution of code until something is fetched
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search',{ 
+            params : {
+                part : 'snippet',
+                maxResults : 5,
+                key : 'AIzaSyDwtw8eSkhwl3rx6_iEgOw4qW3VjJO2MCM',
+                q:searchTerm
+            }
+        });
+
+        console.log(response.data.items);
+    }
+
     render() {
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
                             {/* Search Bar */}
-                            <SearchBar />
+                            <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
                             {/* Video detail */}
--- youtube-clone/src/components/SearchBar.js
+++ youtube-clone/src/components/SearchBar.js
@@ -7,11 +7,26 @@ class SearchBar extends React.Component{
         searchTerm : ''
     }
 
+    //declare a normal function, we have to bind the function in class
+    //if we made it a arrow function, we dont need to bind
+
+    handleChange = (event) => this.setState({searchTerm : event.target.value});
+
+    handleSubmit = (event) => {
+        const {searchTerm} = this.state;
+        const {onFormSubmit} = this.props;
+
+        onFormSubmit(searchTerm);
+
+        event.preventDefault(); 
+    }
+    
     render() {
         return (
             <Paper elevation={6} style={{padding:'25px'}}>
-                <form>
-                    <TextField fullWidth label="Search..."></TextField>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search..." onChange={this.handleChange} />
+                    {/* onChange method in react handles the input changes and changes the state accordingly */}
                 </form>
             </Paper>
         )
