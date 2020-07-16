231546dfec9568aa6f67bfcabcf37c79a0006343
Pankhuri <pankhuritrikha@gmail.com>
2020-07-16 22:57:11 +0530

    Finally

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
             params : {
                 part : 'snippet',
                 maxResults : 5,
-                key : 'AIzaSyDGKeShkGlI_6HEf04oAfJHGaO-_u_D4as',
+                key : 'api_key',
                 q:searchTerm
             }
         });
